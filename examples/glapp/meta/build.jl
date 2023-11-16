a4e01d7212da5e35a8858db6e90be32a723c05e1
Janis Erdmanis <janiserdmanis@protonmail.ch>
2023-11-16 22:40:26 +0200

    Adding glapp for testing mesa libs

---
+++ examples/glapp/meta/build.jl
@@ -0,0 +1,10 @@
+using AppBundler
+
+import Pkg.BinaryPlatforms: Linux, MacOS, Windows
+
+APP_DIR = dirname(@__DIR__)
+
+BUILD_DIR = joinpath(APP_DIR, "build")
+mkpath(BUILD_DIR)
+
+AppBundler.bundle_app(Linux(:x86_64), APP_DIR, "$BUILD_DIR/glapp-x64.snap")
--- examples/qmlapp-strict/meta/precompile.jl
+++ examples/qmlapp-strict/meta/precompile.jl
@@ -1,1 +1,1 @@
-import QML
+#import QML
