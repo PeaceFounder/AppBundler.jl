d5239db12299f5f45ab379ed03afe4558cfce28b
Janis Erdmanis <janiserdmanis@protonmail.ch>
2025-05-12 10:28:37 +0300

    fixing subject field for self signed msix certificates

--- src/Utils/MSIXPack.jl
+++ src/Utils/MSIXPack.jl
@@ -6,6 +6,123 @@ using osslsigncode_jll
 using rcodesign_jll: rcodesign
 using OpenSSL_jll: openssl
 
+function generate_self_signed_certificate(pfx_path; password = "", name = "AppBundler", country = "XX", organization = "PeaceFounder")
+    code_sign_conf = """
+    [ req ]
+    default_bits = 2048
+    prompt = no
+    default_md = sha256
+    distinguished_name = dn
+    req_extensions = req_ext
+
+    [ dn ]
+    CN = $name
+    C = $country
+    O = $organization
+
+    [ req_ext ]
+    keyUsage = digitalSignature
+    extendedKeyUsage = codeSigning
+    """
+
+    conf = joinpath(tempdir(), "code_sign.conf")
+    rm(conf; force=true)
+    write(conf, code_sign_conf)
+
+    private_key = joinpath(tempdir(), "private.key")
+    rm(private_key; force=true)
+    
+    certificate_crt = joinpath(tempdir(), "certificate.crt")
+    rm(certificate_crt; force=true)
+
+    # Generate private key and self-signed certificate
+    run(`$(openssl()) req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout "$private_key" -out "$certificate_crt" -config "$conf"`)
+
+    run(`$(openssl()) pkcs12 -export -out "$pfx_path" -inkey "$private_key" -in "$certificate_crt" -passout "pass:$password"`)
+
+    return
+end
+
+function extract_subject_from_certificate(cert_path; password = "")
+    # Run the OpenSSL command and capture the output
+    cmd = `$(openssl()) x509 -in $cert_path -noout -subject -nameopt RFC2253 -passin "pass:$password"`
+    output = read(cmd, String)
+    
+    # Extract just the subject part
+    if occursin("subject=", output)
+        return replace(output, r"subject= *" => "") |> strip
+    else
+        return output |> strip
+    end
+end
+
+function extract_publisher_from_manifest(appxmanifest_path)
+    # Read the manifest file
+    content = read(appxmanifest_path, String)
+    
+    # Extract the Publisher attribute using regex
+    publisher_match = match(r"Publisher=\"([^\"]*)\"", content)
+    
+    if publisher_match !== nothing
+        return publisher_match.captures[1]
+    else
+        return nothing  # Publisher attribute not found
+    end
+end
+
+function update_publisher_in_manifest(appxmanifest_path, publisher)
+
+    publisher = replace(publisher, ","=>", ")
+
+    # Read the manifest file
+    content = read(appxmanifest_path, String)
+    
+    # Replace the Publisher attribute value using regex
+    updated_content = replace(content, r"Publisher=\"[^\"]*\"" => "Publisher=\"$publisher\"")
+    
+    # Write the updated content back to the file
+    write(appxmanifest_path, updated_content)
+end
+
+function pack2msix(source, destination; pfx_path = nothing, password = "")
+
+    # I could create a self signed certificate from AppxManifest.xml
+    # Need to make CN to match the AppxManifest.xml
+    @assert !isnothing(pfx_path) "Not yet implemented"
+
+    @show extract_subject_from_certificate(pfx_path; password)
+
+    publisher = extract_subject_from_certificate(pfx_path; password)
+    @info "Using publisher: $publisher"
+
+    appxmanifest = joinpath(source, "AppxManifest.xml")
+
+    publisher_manifest = extract_publisher_from_manifest(appxmanifest)
+    
+    if publisher_manifest == ""
+        @info "Setting publisher to $publisher"
+    elseif publisher_manifest != publisher
+        @warn "Publisher in manifest is $publisher_manifest wheras in certificate $publisher. Using the latter"
+    end
+
+    update_publisher_in_manifest(appxmanifest, publisher)
+
+    @info "Forming MSIX archive"
+    unsigned_msix = joinpath(tempdir(), "unsigned_msix.msix")
+    rm(unsigned_msix; force=true)
+
+    run(`$(makemsix()) pack -d $source -p $unsigned_msix`)
+
+    @info "Performing codesigning"
+
+    @info "signed msix at $destination"
+
+    rm(destination; force=true)
+    run(`$(osslsigncode()) sign -pkcs12 $pfx_path -pass "$password" -in "$unsigned_msix" -out "$destination"`)
+
+    return
+end
+
 function extract_msix(archive_path::String)
 
     zip = ZipFile.Reader(archive_path)
@@ -37,89 +154,6 @@ function extract_msix(archive_path::String)
     return output_directory
 end
 
-
-function extract_subject_from_certificate(cert_path; password = "")
-    # Run the OpenSSL command and capture the output
-    cmd = `$(openssl()) x509 -in $cert_path -noout -subject -nameopt RFC2253 -passin "pass:$password"`
-    output = read(cmd, String)
-    
-    # Extract just the subject part
-    if occursin("subject=", output)
-        return replace(output, r"subject= *" => "")
-    else
-        return output
-    end
-end
-
-
-function pack2msix(source, destination; pfx_path = nothing, password = "", replace_publisher = true)
-
-    # I could create a self signed certificate from AppxManifest.xml
-    # Need to make CN to match the AppxManifest.xml
-    @assert !isnothing(pfx_path) "Not yet implemented"
-
-    publisher = strip(extract_subject_from_certificate(pfx_path; password))
-    @info "Using publisher: $publisher"
-
-    appxmanifest = joinpath(source, "AppxManifest.xml")
-    run(`sed -i '' "s/Publisher=\"[^\"]*\"/Publisher=\"$publisher\"/g" "$appxmanifest"`)
-
-
-    @info "Forming MSIX archive"
-
-    unsigned_msix = joinpath(tempdir(), "unsigned_msix.msix")
-    rm(unsigned_msix; force=true)
-
-    run(`$(makemsix()) pack -d $source -p $unsigned_msix`)
-
-    @info "Performing codesigning"
-
-    @info "signed msix at $destination"
-
-    rm(destination; force=true)
-    run(`$(osslsigncode()) sign -pkcs12 $pfx_path -pass "$password" -in "$unsigned_msix" -out "$destination"`)
-
-    return
-end
-
-
-function generate_self_signed_certificate(pfx_path; password = "")
-    code_sign_conf = """
-    [ req ]
-    default_bits = 2048
-    prompt = no
-    default_md = sha256
-    distinguished_name = dn
-    req_extensions = req_ext
-
-    [ dn ]
-    CN = AppBundler
-
-    [ req_ext ]
-    keyUsage = digitalSignature
-    extendedKeyUsage = codeSigning
-    """
-
-    conf = joinpath(tempdir(), "code_sign.conf")
-    rm(conf; force=true)
-    write(conf, code_sign_conf)
-
-    private_key = joinpath(tempdir(), "private.key")
-    rm(private_key; force=true)
-    
-    certificate_crt = joinpath(tempdir(), "certificate.crt")
-    rm(certificate_crt; force=true)
-
-    # Generate private key and self-signed certificate
-    run(`$(openssl()) req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout "$private_key" -out "$certificate_crt" -config "$conf"`)
-
-    run(`$(openssl()) pkcs12 -export -out "$pfx_path" -inkey "$private_key" -in "$certificate_crt" -passout "pass:$password"`)
-
-    
-    return
-end
-
-
 # A helper function to explore potential issuess with msixpack
 function repack(source, destination; pfx_path = nothing, publisher = nothing, password = "")
 
@@ -131,19 +165,9 @@ function repack(source, destination; pfx_path = nothing, publisher = nothing, pa
         pfx_path = joinpath(tempdir(), "certificate.pfx")
 
         password = "YourPassword"
-        publisher = "CN=AppBundler"
         generate_self_signed_certificate(pfx_path; password)
         
     end
-
-    # CN=YourName, O=YourOrg, C=YourCountry
-
-    # if !isnothing(publisher)
-
-    #     appxmanifest = joinpath(extracted_msix, "AppxManifest.xml")
-    #     run(`sed -i '' "s/Publisher=\"[^\"]*\"/Publisher=\"$publisher\"/g" "$appxmanifest"`)
-
-    # end
 
     pack2msix(extracted_msix, destination; pfx_path, password)
 
