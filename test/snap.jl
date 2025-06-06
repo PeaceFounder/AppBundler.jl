c97e0c55529aae05ddd1349082cc75b5f05d3daf
Janis Erdmanis <janiserdmanis@protonmail.ch>
2025-06-06 13:10:16 +0300

    adding prefix for snap precompilation test

--- test/snap.jl
+++ test/snap.jl
@@ -2,6 +2,6 @@ import AppBundler: build_app
 import Pkg.BinaryPlatforms: Linux
 
 src_dir = joinpath(dirname(@__DIR__), "examples/gtkapp")
-snap_path = joinpath(homedir(), "Desktop/gtkapp.snap")
+snap_path = joinpath(tempdir(), "gtkapp.snap")
 
 build_app(Linux(Sys.ARCH), src_dir, snap_path; precompile = Sys.islinux())
